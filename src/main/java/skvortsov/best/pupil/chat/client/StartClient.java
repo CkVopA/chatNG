ed1375ff0f15d8a98af3a7386b1bb2c176c049b8
Igor <bird-starling@ya.ru>
2022-03-09 20:12:37 +0300

    add methods in controller

--- src/main/java/skvortsov/best/pupil/chat/client/StartClient.java
+++ src/main/java/skvortsov/best/pupil/chat/client/StartClient.java
@@ -2,6 +2,7 @@ package skvortsov.best.pupil.chat.client;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -11,10 +12,21 @@ public class StartClient extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StartClient.class.getResource("chat-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Chat NextGEN!");
         stage.setScene(scene);
         stage.show();
+    }
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(StartClient.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+    public static void windowAbout(Stage stageAbout) throws IOException {
+        Scene sceneAbout = new Scene(loadFXML("aboutWindow"));
+        stageAbout.setScene(sceneAbout);
+        stageAbout.setTitle("About APP");
+        stageAbout.centerOnScreen();
+        stageAbout.show();
     }
 
     public static void main(String[] args) {
--- src/main/java/skvortsov/best/pupil/chat/client/controllers/ChatController.java
+++ src/main/java/skvortsov/best/pupil/chat/client/controllers/ChatController.java
@@ -1,14 +1,78 @@
 package skvortsov.best.pupil.chat.client.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import skvortsov.best.pupil.chat.client.StartClient;
 
-public class ChatController {
-    @FXML
-    private Label welcomeText;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ChatController implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField inputField;
+
+    @FXML
+    private TextArea chatList;
+
+    @FXML
+    private ListView<String> listContacts;
+
+    private final ObservableList<String> contacts = FXCollections.observableArrayList(
+            "Senior","Middle","Junior"
+    );
+
+    @FXML
+    private TextField fieldNewContact;
+
+    @FXML
+    public void sendMessageToChatList(){
+        String msg = inputField.getText();
+        inputField.clear();
+        if (!msg.isBlank()) {
+            appendMessage(msg);
+        }
+    }
+
+    private void appendMessage(String msg) {
+        //      try {
+        //          out.writeUTF(msg);
+        //      }
+        //      catch (IOException e) {
+        //          e.printStackTrace();
+        //      }
+        chatList.appendText(msg + "\n");
+    }
+
+    @FXML
+    public void clearChatList(){
+        chatList.clear();
+    }
+
+    @FXML
+    public void exitApp(){
+        System.exit(1);
+    }
+
+    @FXML
+    public void about() throws IOException {
+        StartClient.windowAbout(new Stage());
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        listContacts.setItems(contacts);
+
     }
 }
